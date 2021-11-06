93ad4cead4bdfc5482a21366022499cd5833b739
NaomiGMartinez <90790343+NaomiGMartinez@users.noreply.github.com>
2021-11-05 22:44:16 -0500

    Update QR.py
    for HW 06, I edited QR from HW 05 and added the new function to QR while deleting the unstable Gram-Shmidt

--- QR.py
+++ QR.py
@@ -1,35 +1,25 @@
 """
-This assignment is due by 11:59pm on 10/29/2021. 
+This assignment is due by 11:59pm on 11/05/2021. 
 
-For this assignment you will be writing a python script named QR.py which will
-contain several functions. All functions must satisfy the same requirements as in HW03. 
+For this assignment you will be updating the python script QR.py from the
+previous homework. As usual, all functions must satisfy the same requirements as in HW03. 
 
 You will import the LA.py script from HW03 and HW04. You must make use of those
 functions to implement the functions below. Failure to do this will result in an
 earned grade of 0.
 
-For all functions below, matrices will be stored as lists of lists where each
-component list represents a column of the matrix. Use of any other
-representation will result in an earned grade of 0.
+1) Remove the function which implemented unstable Gram-Schmidt. It is unstable
+and we may use the stable version exclusively from this point forward. 
 
-The functions you will need to add are
-
-1) A function which implements the unstable version of Gram-Schmidt for reduced QR
-factorization. It will
-take as it's argument a matrix and will return a list of two matrices. The first
-will be Q and the second will be R from the QR factorization described in the
-algorithm.
-
-2) A function which implements the stable version of Gram-Schmidt for reduced QR
-factorization.It will take as it's argument a matrix and will return a list of 
-two matrices. The first will be Q and the second will be R from the QR factorization 
-described in the algorithm.
-
+2) Write a function which takes as it's argument a list of vectors and returns
+an orthonormal list of vectors which shares the same span. 
 """
+
 from LA import *
 matrix_a = [[1, 1, 1], [1, 1, 0], [1, 0, 0]]
 
-def gram_schmidt_unstable(matrix):
+#Using the unstable version from my previous code since my other one was not quite finished
+def gram_schmidt_stable(matrix):
     """Returns the QR decomposition of a matrix
 
     The function first calculates Q by normalizing the input.
@@ -47,6 +37,7 @@ Returns:
     Q = []
     #Calculate Q matrix
     for column in range(columns):
+        #Calculate orthogonal vector
         i = columns - column - 1
         qcolumn = matrix[column]
         while(i < (columns - 1)):
@@ -57,6 +48,7 @@ Returns:
             for k in range(rows):
                 qcolumn[k] = qcolumn[k] - pvector[k]
             i = i + 1
+        #Normalize the vector
         pnorm = p_norm(qcolumn)
         qcolumn = scalar_vector_multiplication((1/pnorm), qcolumn)
         Q.append(qcolumn)
@@ -76,72 +68,37 @@ Returns:
                 
     return [Q, R]
 
-def gram_schmidt_stable(matrix):
-    """Returns the QR decomposition of a matrix
+def orthonormal_vectors(matrix):
+    """Returns an orthonormal list of vectors
 
-    The function first creates an orthogonal matrix "v" from
-    the original matrix. It then calculates Q by normalizing v.
-    The function then calculates R by getting the product of
-    the transpose of Q by the original matrix
+    Given that a list of vectors is essentially a matrix
+    and QR decomposition returns a normalized orthogonal
+    matrix (assuming dimensions are the same), this is
+    a modified version of the previous gram schmidt code
 Args:
-    matrix: A matrix of numbers stored as a list of lists
-            The first dimension is the columns
+    matrix: A matrix of numbers stored as a list of vectors
 
 Returns:
-   A list containing Q and R respectively
+   A list containing orthonormal vectors
 """ 
     columns = len(matrix)
     rows = len(matrix[0])
-    v = []
-    #Create orthogonal matrix "v"
-    for column in range(columns):
-        vcolumn = matrix[column]
-        i = columns - column
-        while(i > 0):
-            numerator = inner_product(matrix[0], vcolumn)
-            denominator = inner_product(matrix[0], matrix[0])
-            #!!!Getting ZeroDivisionError even though denominator is not equal to 0!!!
-            projection = numerator/denominator
-            pvector = scalar_vector_multiplication(projection, matrix[0])
-            for j in range(rows):
-                vcolumn[j] = vcolumn[j] - pvector[j]
-        v.append(vcolumn)
-    #Calculate the Q matrix    
     Q = []
+    #Calculate Q matrix
     for column in range(columns):
-        #Calculate the norm of each column in orthogonal matrix
-        Qn = v[column]
-        Qn = scalar_vector_multiplication(1/p_norm(v[column]), Qn)
-        #Append each normalized column into Q
-        Q.append(Qn)
-
-    R = [[0 for i in range(rows)] for j in range(columns)]
-    for i in range(columns):
-        for j in range(rows):
-            #Sets 0's in lower triangle
-            if j > i:
-                R[i][j] = 0
-            #Not sure if this always works, just something noticed during testing.
-            elif i > j:
-                R[i][j] = Q[j][columns - i - 1]
-            #Sets R[i][j] to inner product of
-            else:
-                R[i][j] = inner_product(matrix[i], Q[j])
-                
-    return [Q, R]
-    
-        
-QRD = (gram_schmidt_unstable([[1,1,0],[1,0,1],[0,1,1]]))
-print(QRD[0])
-print(QRD[1])
-print("\n")
-
-def test1():
-    assert gram_schmidt_unstable(matrix_a) == [[[0.7071067811865475, 0.7071067811865475, 0.0], [0.4082482904638632, -0.40824829046386296, 0.8164965809277261], [-0.5773502691896257, 0.5773502691896258, 0.5773502691896257]], 
-                                               [[1.414213562373095, 0, 0], [0.7071067811865475, 1.2247448713915892, 0], [0.7071067811865475, 0.4082482904638632, 1.1547005383792515]]]
-
-
-#print(gram_schmidt_stable(matrix_a))
-#Proof that inner_product does not return 0
-#Therefore could not identify cause of ZeroDivisionError
-print(inner_product(matrix_a[0], matrix_a[0]))
+        #Calculate orthogonal vector
+        i = columns - column - 1
+        qcolumn = matrix[column]
+        while(i < (columns - 1)):
+            j = columns - i
+            j = j - 1
+            product = inner_product(matrix[column], Q[column - j]) 
+            pvector = scalar_vector_multiplication(product, Q[column - j])
+            for k in range(rows):
+                qcolumn[k] = qcolumn[k] - pvector[k]
+            i = i + 1
+        #Normalize the vector
+        pnorm = p_norm(qcolumn)
+        qcolumn = scalar_vector_multiplication((1/pnorm), qcolumn)
+        Q.append(qcolumn)
+    return Q
